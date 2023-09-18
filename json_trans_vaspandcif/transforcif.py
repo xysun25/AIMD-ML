79ca84adcddf7393611e2696990051807ed4c892
xysun25 <sunxinyuexiannv@163.com>
2023-09-18 15:15:34 +0800

    Update transforcif.py

--- json_trans_vaspandcif/transforcif.py
+++ json_trans_vaspandcif/transforcif.py
@@ -1,5 +1,5 @@
 from ase.io import read, write
 
-atoms = rea('structure.json', format = 'json')
+atoms = read('structure.json', format = 'json')
 write('struc.cif', atoms)
 write('struc.vasp', atoms)
