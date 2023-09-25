73ac7f497b72f2f45d5ca254513222c283aebcf1
xysun25 <sunxinyuexiannv@163.com>
2023-09-25 20:14:38 +0800

    Update transforcif.py

--- json_trans_vaspandcif/transforcif.py
+++ json_trans_vaspandcif/transforcif.py
@@ -8,6 +8,7 @@ from ase.io import read, write
 
 # 从 Trajectory 文件中读取结构
 traj = read('aimd_with_bader.traj', index=':', format='traj')
+
 # # 将每个帧写入单独的 XYZ 文件
 # for i, atoms in enumerate(traj):
 #     output_file = f'output_{i}.xyz'
